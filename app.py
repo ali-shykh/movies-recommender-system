1a292082004b6d64079b038c06075d439e8116b9
ali-shykh <alishykh000@gmail.com>
2023-12-11 13:08:15 +0500

    commit

--- app.py
+++ app.py
@@ -3,20 +3,22 @@ import pickle
 import pandas as pd
 import requests
 
-movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+with open("styles.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 def feth_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=194f2f32edc6b9c9d3cfbd6c9831ec40&language=en-US'.format(movie_id))
     data = response.json()
-    print(data)
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     if movie not in movies['title'].values:
-        print(f"{movie} not found in the DataFrame.")
+        st.warning(f"{movie} not found in the DataFrame.")
         return
 
     index = movies[movies['title'] == movie].index[0]
@@ -24,12 +26,16 @@ def recommend(movie):
 
     recommended_movies = []
     recommended_movies_posters = []
+    recommended_movies_tags = []
+
     for i in distances[1:6]:
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch posters from API
         recommended_movies_posters.append(feth_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
+        recommended_movies_tags.append(movies.iloc[i[0]].tags)
+
+    return recommended_movies, recommended_movies_posters, recommended_movies_tags
+
 
 st.title("Movies Recommender System")
 
@@ -37,28 +43,32 @@ selected_movie_name = st.selectbox(
     'Pick a movie for recommendation',
     movies['title'].values)
 
-
 if st.button('Recommend'):
-    names,posters = recommend(selected_movie_name)
+    names, posters, tags = recommend(selected_movie_name)
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
         st.text(names[0])
         st.image(posters[0])
+        st.write("Tags:", tags[0])
 
     with col2:
         st.text(names[1])
         st.image(posters[1])
+        st.write("Tags:", tags[1])
 
     with col3:
         st.text(names[2])
         st.image(posters[2])
+        st.write("Tags:", tags[2])
 
     with col4:
         st.text(names[3])
         st.image(posters[3])
+        st.write("Tags:", tags[3])
 
     with col5:
         st.text(names[4])
         st.image(posters[4])
+        st.write("Tags:", tags[4])
